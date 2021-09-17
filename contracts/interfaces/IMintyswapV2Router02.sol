6913e830bb3e4b0c7ab03de3e21a2fc4bae516a3
top1st <riguang102@gmail.com>
2021-09-17 23:34:42 +0800

    mintyswap renames

--- contracts/interfaces/IMintyswapV2Router02.sol
+++ contracts/interfaces/IMintyswapV2Router02.sol
@@ -1,8 +1,8 @@
 pragma solidity >=0.6.2;
 
-import './IUniswapV2Router01.sol';
+import './IMintyswapV2Router01.sol';
 
-interface IUniswapV2Router02 is IUniswapV2Router01 {
+interface IMintyswapV2Router02 is IMintyswapV2Router01 {
     function removeLiquidityETHSupportingFeeOnTransferTokens(
         address token,
         uint liquidity,
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,10 +4,18 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
-const {ethers} = hre
+const {ethers, network} = hre
 require('dotenv').config()
 
 const deployer = new ethers.Wallet(process.env.PRIVATE_KEY)
+
+const factoryAddress = "0xA3BceDADB23CfAA592aC42f5A18C98F98a37dA77"
+
+const WETHAddress = {
+    'hardhat': '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+    'mainnet': '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    'rinkeby': '0xc778417E063141139Fce010982780140Aa0cD5Ab'
+}[network.name]
 
 async function main() {
     // Hardhat always runs the compile task when running scripts with its command
@@ -17,13 +25,14 @@ async function main() {
     // manually to make sure everything is compiled
     // await hre.run('compile');
 
+    const factory = await hre.ethers.getContractAt("IMintyswapV2Factory", "factoryAddress", deployer)
     // We get the contract to deploy
-    const Router = await hre.ethers.getContractFactory("Router");
-    const greeter = await Greeter.deploy("Hello, Hardhat!");
+    const Router = await hre.ethers.getContractFactory("MintyswapV2Router02", deployer);
+    const router = await Router.deploy(factory.address, WETHAddress);
 
-    await greeter.deployed();
+    await router.deployed();
 
-    console.log("Greeter deployed to:", greeter.address);
+    console.log("router deployed to:", router.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
